e14fd91aacc212ee557007949e391d9700129537
Garly Nugraha <98641173+garllockhart@users.noreply.github.com>
2022-02-25 20:18:59 +0700

    Add Program PTR15

---
+++ Pointer/PTR15/PTR15.cpp
@@ -0,0 +1,39 @@
+/*
+File Name		: PTR15.cpp
+Made by			: Tim Dosen SDA
+Created Date	: 13/09/2001
+Edited by		: Garly Nugraha
+Edited Date		: 24/02/2022
+Description		: 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	/* kamus */
+	/* Definisi array yang elemennya string, statik dan sekaligus mengisi */
+	static char *s[3] = {"the", "time", "is"};
+	/* Definisi array yang elemennya string, dinamik */
+	char *(*TabStr); /* Deklarasi array of string */
+	int i;
+	
+	/* program */
+	for (i=0; i < 3; i++)
+	{ //cetak isi s
+		printf("%s\n", s[i]);
+	}
+	
+	/* Alokasi TabStr sebanyak 3 */
+	TabStr = (char **) malloc (3 * sizeof(char *));
+	for (i=0; i < 3; i++) {
+		/* Alokasi string yang merupakan elemen tabel */
+		*(TabStr + i) = (char *) malloc (5 * sizeof(char));
+		printf("\nInput Str[%d], maksimum 5 karakter : ", i);
+		scanf("%5s", *(TabStr+i)); /* Mengisi nilai string */
+		printf("\n Nilai Str[%d] : %5s\n ", i, *(TabStr + i));
+	}
+	
+	return 0;
+}
+
