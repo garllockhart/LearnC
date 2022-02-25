8867323ee3f6deedb11b28155c130cba7ee12d1c
Garly Nugraha <98641173+garllockhart@users.noreply.github.com>
2022-02-25 20:18:34 +0700

    Add Program PTR14

---
+++ Pointer/PTR14/PTR14.cpp
@@ -0,0 +1,44 @@
+/*
+File Name		: PTR14.cpp
+Made by			: Tim Dosen SDA
+Created Date	: 13/09/2001
+Edited by		: Garly Nugraha
+Edited Date		: 24/02/2022
+Description		: 
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	/* Kamus Data (deklarasi, inisialisasi nilai variabel)*/
+	int *tab; /* deklarasi array, perhatikan belum tampak bedanya dg
+	deklarasi pointer biasa */
+	int N, i;
+	
+	/* program */
+	printf("Contoh mengisi array dinamis berukuran 0..N : \n"); // 
+	printf("N = "); // 
+	scanf("%d", &N); // 
+	printf("Alokasi setelah mengetahui ukuran array \n"); // 
+	tab = (int *) malloc((N+1) *sizeof(int)); // 
+	
+	if (tab != NULL) {
+		for (i=0; i<=N; i++)
+		{
+			printf("i=%d tab[i]=%d \n", i, *(tab + i));
+		};
+		
+		printf("Akhir program \n");
+		
+		/* dealloc */
+		free(tab); /*dealokasi*/
+		return 0;
+	} else {
+		printf("Alokasi gagal ... \n");
+		return 1;
+	}
+	
+	return 0;
+}
